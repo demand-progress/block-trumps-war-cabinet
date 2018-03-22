b9e0fce61bb3b315cc41729cfdce7b26d182317e
Mateo Balcorta <misterrattail@gmail.com>
2018-03-22 09:19:00 -0700

    Commented out logos.

--- js/containers/Footer.jsx
+++ js/containers/Footer.jsx
@@ -8,11 +8,11 @@ class Footer extends Component {
             <div className="footer">
                 <div className="logos-unit">
                     <div className="built-by">
-                        <p><br/><br/>Built by:</p> <img src="images/demand-progress.png" />
+                        <p><br/><br/>Built by:</p> <img src="../images/demand-progress.png" />
                         <p style={{marginBottom: "50px"}}><br/><br/>Participating organizations:</p>  
                           <div className="logos" style={{display: "flex", flexFlow: "row wrap", justifyContent: "center", alignItems: "center", marginTop: "-50px", borderBottomColor:"white"}}>
             
-                             <a target="_blank">
+                             {/* <a target="_blank">
                                   <img alt="About Face" src="images/About Face Logo.jpg" />
                               </a>
 
@@ -75,12 +75,12 @@ class Footer extends Component {
                               <a target="_blank">
                                 <img alt="World Beyond War" src="images/World Beyond War 300x150_preview.jpeg" />
                               </a>
-                              
+                               */}
                           </div>
                           <p style={{marginBottom: "50px"}}>Endorsing organizations:</p> 
                           <div className="logos" style={{display: "flex", flexFlow: "row wrap", justifyContent: "center", alignItems: "center", marginTop: "-50px", borderBottomColor:"white"}}>
                              
-                             <a target="_blank">
+                             {/* <a target="_blank">
                                   <img alt="Anti War" src="images/Antiwarcom logo.png" />
                               </a>
 
@@ -106,7 +106,7 @@ class Footer extends Component {
                   
                               <a target="_blank">
                                   <img alt="The Yemen Peace Project" src="images/YPP-logo_preview.png" />
-                              </a>
+                              </a> */}
                           </div>
                       </div> 
                     <div className="spacer"></div>
@@ -124,11 +124,11 @@ class Footer extends Component {
                         </div>
                         <div className="social-media">
                             <a className="twitter" href="https://twitter.com/intent/tweet?text=Our%20public%20dollars%20are%20fueling%20the%20worst%20humanitarian%20crisis%20on%20the%20globe.%20It%27s%20up%20to%20us%20to%20stop%20it.%20Urge%20your%20Sen%20to%20cosponsor%20%23SandersLeeYemen%20resolution%20%26%20cut%20off%20U.S.%20support%20for%20Saudi%20Arabia%27s%20brutal%20war%20in%20%23Yemen.%20www.Stopthewar.us" target="_blank">
-                                <img src="images/twitter_white.svg" />
+                                <img src="../images/twitter_white.svg" />
                                 <span>Share on twitter</span>
                             </a>
                             <a className="facebook" href="https://www.facebook.com/sharer.php?u=https://stopthewar.us/" target="_blank">
-                                <img src="images/facebook_white.svg" />
+                                <img src="../images/facebook_white.svg" />
                                 <span>Share on facebook</span>
                             </a>
                         </div>
