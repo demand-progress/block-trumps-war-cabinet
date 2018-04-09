d82536ee1ff3cbd04a0307baf023c960baefb516
Mateo Balcorta <misterrattail@gmail.com>
2018-04-09 10:16:25 -0700

    Remove header text.

--- js/containers/ActionForm.jsx
+++ js/containers/ActionForm.jsx
@@ -131,7 +131,6 @@ class ActionForm extends Component {
       
       return(
         <div className="bftn-form call-action-form">
-            <h4>Sign to email the senate:</h4>
             <h3>Block Trump's War Cabinet</h3>
             <br/><br/>
             <div style={{color: 'white', lineHeight: 1.5}}>
